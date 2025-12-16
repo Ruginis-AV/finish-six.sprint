0b1f936f3031cddf30046c2bf0dadc7c8f4a9556
Ruginis-AV <ruginis2002@gmail.com>
2025-12-16 20:01:40 +0300

    create code

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"finish-six.sprint/internal/handlers"
+	"finish-six.sprint/internal/server"
+	"finish-six.sprint/internal/service"
+	"finish-six.sprint/pkg/morse"
+)
+
+func main() {
+	log := log.New(os.Stdout, "", 0)
+
+	converter := morse.NewConverter(morse.DefaultMorse)
+
+	mainService := service.New(converter)
+
+	mainHandler := handlers.New(log, mainService)
+
+	serv := server.New(log, "localhost", "8080", 10, 5, 15, mainHandler)
+
+	serv.Start()
 }
